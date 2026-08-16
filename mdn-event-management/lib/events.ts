6b1fa26d06ec6f100b83496453cee7e0b57fd1cf
dbys0001 <dbys0001@student.monash.edu>
2026-08-16 17:00:58 +1000

    wip: Event Creation function Implemented createEvent function with a static input format. Also updated some dependencies.

---
+++ mdn-event-management/lib/events.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/lib/prisma";
+
+type createEventInput = {
+    name: string;
+    description: string;
+    date: Date;
+    locationId: number;
+    managerIds?: number[];
+}
+
+export async function createEvent(input: createEventInput) {
+    return prisma.event.create({
+        data: {
+            name: input.name,
+            description: input.description,
+            date: input.date,
+            location: { connect: { locationId: input.locationId }},
+            bookable: { create: { bookableType: "Event" }},
+            eventManagers: input.managerIds?.length
+                ? { create: input.managerIds.map((memberId: number) => ({ memberId }))}
+                : undefined,
+        },
+        include: {
+            location: true,
+            bookable: true,
+            eventManagers: { include: {member: true}}
+        }
+    });
+}
+
+export async function readEvents() {}
+
+export async function updateEvent() {}
+
+export async function deleteEvent() {}

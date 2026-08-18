ec6f3bbcfdafec3c6482bf70260600c03bbe678f
dbys0001 <dbys0001@student.monash.edu>
2026-08-18 17:34:28 +1000

    wip: Added update (PATCH) call for events
    Added logic to update an event through API.
    Also added some appropriate error handling for the function.

--- mdn-event-management/app/api/events/[eventId]/route.ts
+++ mdn-event-management/app/api/events/[eventId]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { readEvent, deleteEvent, updateEvent } from "@/lib/events";
+import { Prisma } from "@/generated/prisma/client";
 
 type RouteParams = {
     params: Promise<{ eventId: string }>;
@@ -24,7 +25,37 @@ export async function GET(request: Request, context: RouteParams) {
 }
 
 export async function PATCH(request: Request, context: RouteParams) {
+    // Extract the eventId from the URL parameters
+    const { eventId } = await context.params;
+    const id = Number(eventId);
 
+    // Extract the params to update event with
+    const body = await request.json();
+
+    try {
+        // Update the event
+        const event = await updateEvent(id, {
+            name: body.name,
+            description: body.description,
+            date: body.date !== undefined ? new Date(body.date) : undefined,
+            locationId: body.locationId !== undefined ? Number(body.locationId) : undefined,
+        });
+
+        // Return the updated event
+        return NextResponse.json(event, { status: 200 });
+    } catch (error) {
+        if (
+            error instanceof Prisma.PrismaClientKnownRequestError
+        ) {
+            if (error.code === 'P2025') {
+                return NextResponse.json({ error: "Event not found" }, { status: 404 });
+            }
+
+            return NextResponse.json({ error: "Failed to update event" }, { status: 500 });
+        }
+
+        return NextResponse.json({ error: "Failed to update event" }, { status: 500 });
+    }
 }
 
 export async function DELETE(request: Request, context: RouteParams){

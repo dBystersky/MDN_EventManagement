03d4010c31b519b3417eec9fd0bf7c5d6b95d5d8
dbys0001 <dbys0001@student.monash.edu>
2026-08-18 17:18:04 +1000

    wip: Added GET for individual event
    When given the event ID in the URL, GET request returns event json object.

--- mdn-event-management/app/api/events/[eventId]/route.ts
+++ mdn-event-management/app/api/events/[eventId]/route.ts
@@ -1,2 +1,32 @@
 import { NextResponse } from "next/server";
 import { readEvent, deleteEvent, updateEvent } from "@/lib/events";
+
+type RouteParams = {
+    params: Promise<{ eventId: string }>;
+}
+
+// Functino to get individual event from the API
+export async function GET(request: Request, context: RouteParams) {
+    // Extract the eventId from the URL parameters
+    const { eventId } = await context.params;
+    const id  = Number(eventId);
+
+    // Get the event from the database
+    const event = await readEvent(id);
+
+    // If the event is not found, return a 404 error
+    if (!event) {
+        return NextResponse.json({ error: "Event not found" }, { status: 404 });
+    }
+
+    // Return the found event
+    return NextResponse.json(event, { status: 200 });
+}
+
+export async function PATCH(request: Request, context: RouteParams) {
+
+}
+
+export async function DELETE(request: Request, context: RouteParams){
+
+}

daddbb05c344ed1818a3156f5cb5abd435c5d469
dbys0001 <dbys0001@student.monash.edu>
2026-08-19 19:20:23 +1000

    wip: Added api call to get specific location

--- mdn-event-management/app/api/locations/[locationId]/route.ts
+++ mdn-event-management/app/api/locations/[locationId]/route.ts
@@ -1,7 +1,24 @@
 import { NextResponse } from "next/server";
+import { getLocation } from "@/lib/locations";
 
-export async function GET() {
+type RouteParams = {
+    params: Promise<{ locationId: string }>;
+}
 
+export async function GET(request: Request, context: RouteParams) {
+        // Extract the eventId from the URL parameters
+        const { locationId } = await context.params;
+        const id  = Number(locationId);
+    
+        try {
+            // Get the event from the database
+            const event = await getLocation(id);
+    
+            // Return the found event
+            return NextResponse.json(event, { status: 200 });
+        } catch (error) {
+            return NextResponse.json({ error: `Location not found: ${error}` }, { status: 404 }); 
+        }
 }
 
 export async function PATCH() {
